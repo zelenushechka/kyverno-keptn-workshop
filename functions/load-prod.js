63a7152e74579dea07391613324db797cea17ddd
Christian Heckelmann <christian.heckelmann@dynatrace.com>
2024-06-11 08:27:51 +0000

    chore: Update load.js file to use load-dev.js for load testing in dev environment

---
+++ functions/load-prod.js
@@ -0,0 +1,12 @@
+import { sleep } from"k6";
+import http from "k6/http";
+
+export let options = {
+  duration: "1m",
+  vus: 50,
+};
+
+export default function() {
+  http.get("http://demo-app.demo-app-prod.svc.cluster.local:8080/");
+  sleep(1);
+};
