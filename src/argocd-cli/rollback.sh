bf7838cb2efd667827e307a2268b8699dbccfc3a
Christian Heckelmann <89777493+heckelmann@users.noreply.github.com>
2024-06-12 07:29:44 +0000

    diable autosync

--- src/argocd-cli/rollback.sh
+++ src/argocd-cli/rollback.sh
@@ -9,6 +9,10 @@ fi
 # Read secret from /etc/argosecret
 secret=$(cat /etc/argosecret/password)
 
+# Disable autosync
+kubectl -n argocd patch --type='merge' application kyverno-keptn-workshop -p "{\"spec\":{\"syncPolicy\":null}}"
+kubectl -n argocd patch --type='merge' application $1 -p "{\"spec\":{\"syncPolicy\":null}}"
+
 # Login to ArgoCD server
 argocd login argocd-server.argocd.svc.cluster.local --insecure --username admin --password "$secret"
 
